984ff7caad14ce450f205456109926c3a5ca0fde
Babkina-Ekaterina <bkatya5577@yandex.ru>
2024-05-11 00:01:53 +0300

    Change configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         REACT_APP_USERNAME: process.env.REACT_APP_USERNAME,
         REACT_APP_PASSWORD: process.env.REACT_APP_PASSWORD,
-        REACT_APP_SERVER_URL: process.env.REACT_APP_SERVER_URL
+        REACT_APP_SERVER_URL: process.env.REACT_APP_SERVER_URL,
+        REACT_APP_IS_AUTH: process.env.REACT_APP_IS_AUTH
     },
 };
--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import GeneratorPage from './pages/GeneratorPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<GeneratorPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
